19ddbc49ff24d133a06c66ba956d9ae640edf97a
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-01-06 20:07:29 +0100

    Add mouse camera rotation

--- features/player/player.gd
+++ features/player/player.gd
@@ -1,18 +1,30 @@
 class_name Player extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED : float = 2.5
+
+
+func _ready() -> void:
+
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event: InputEvent) -> void:
+
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+
+		rotate_y(-event.relative.x * 0.001)
+		%Camera.rotate_x(-event.relative.y * 0.001)
+		%Camera.rotation.x = clampf(%Camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir : Vector2 = Input.get_vector("walk_left", "walk_right", "walk_forward", "walk_backward")
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	DebugPanel.add_property(input_dir, "input_dir", 1)
 	DebugPanel.add_property(direction, "direction", 2)
