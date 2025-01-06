47d6e2972c1d8ec5180ad9246f1074b924eaff10
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-01-06 20:18:35 +0100

    Add option to hide/show debug panel

--- features/debug_panel/debug_panel.gd
+++ features/debug_panel/debug_panel.gd
@@ -12,6 +12,15 @@ func _ready() -> void:
 	label_settings.outline_size = 10
 
 
+func _unhandled_input(event: InputEvent) -> void:
+
+	if event is InputEventKey and event.pressed and not event.echo:
+
+		if event.physical_keycode == KEY_F1:
+
+			visible = not visible
+
+
 func add_property(value: Variant, display_name: String, order: int) -> void:
 
 	if %LabelsContainer.get_node_or_null(display_name) == null:
