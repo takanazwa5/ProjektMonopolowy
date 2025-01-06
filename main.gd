6c46894f63d0a698e7a757d8a9696521f99bb970
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-01-06 20:07:43 +0100

    Add game quit with esc during gameplay

--- main.gd
+++ main.gd
@@ -4,3 +4,12 @@ class_name Main extends Node
 func _ready() -> void:
 
 	print("main scene loaded")
+
+
+func _unhandled_input(event: InputEvent) -> void:
+
+	if event is InputEventKey and event.pressed and not event.echo:
+
+		if event.physical_keycode == KEY_ESCAPE:
+
+			get_tree().quit()
