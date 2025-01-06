0373dd4dfc88d1036be28156149a81dd0a10685f
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-01-06 19:37:33 +0100

    Add debug panel autoload

---
+++ features/debug_panel/debug_panel.gd
@@ -0,0 +1,28 @@
+class_name DebugPanel extends CanvasLayer
+
+
+var label_settings : LabelSettings = LabelSettings.new()
+
+
+func _ready() -> void:
+
+	show()
+
+	label_settings.outline_color = Color.BLACK
+	label_settings.outline_size = 10
+
+
+func add_property(value: Variant, display_name: String, order: int) -> void:
+
+	if %LabelsContainer.get_node_or_null(display_name) == null:
+
+		var label : Label = Label.new()
+		label.name = display_name
+		label.label_settings = label_settings
+		%LabelsContainer.add_child(label)
+
+	elif visible:
+
+		var label : Label = %LabelsContainer.get_node(display_name)
+		label.text = "%s: %s" % [display_name, value]
+		%LabelsContainer.move_child(label, order)
