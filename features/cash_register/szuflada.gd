2ccd4400226ec6c7a3afbb6d8d59d39550e36e77
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-03-31 15:15:04 +0200

    Change scene hierarchy in cash register

--- features/cash_register/szuflada.gd
+++ features/cash_register/szuflada.gd
@@ -4,6 +4,9 @@ class_name CashRegisterDrawer extends Interactable
 var open : bool = false
 
 
+@onready var szuflada: Node3D = %szuflada
+
+
 func interact() -> void:
 
 	if open:
@@ -18,12 +21,12 @@ func interact() -> void:
 func _open_drawer() -> void:
 
 	var tween : Tween = create_tween().set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, ^"position:z", 0.25, 0.25)
+	tween.tween_property(szuflada, ^"position:z", 0.25, 0.25)
 	open = true
 
 
 func _close_drawer() -> void:
 
 	var tween : Tween = create_tween().set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, ^"position:z", 0.0, 0.25)
+	tween.tween_property(szuflada, ^"position:z", 0.0, 0.25)
 	open = false
