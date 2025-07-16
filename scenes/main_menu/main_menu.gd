7df141ab1324002957e9d7587e7646e6e4781176
takanazwa5 <32649717+takanazwa5@users.noreply.github.com>
2025-07-16 10:05:44 +0200

    Refactor main menu scene transition logic
    Replaces the preloaded GAME_SCENE constant with dynamic loading in the start button handler. Adds error handling and logging for scene transitions, and removes setting the mouse mode.

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,9 +1,6 @@
 class_name MainMenu extends Control
 
 
-const GAME_SCENE : PackedScene = preload("uid://daih55gqatj4l")
-
-
 @onready var start_button: Button = %StartButton
 @onready var quit_button: Button = %QuitButton
 @onready var beer_can_opening_sound: AudioStreamPlayer = %BeerCanOpeningSound
@@ -17,10 +14,20 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	beer_can_opening_sound.play()
-	await beer_can_opening_sound.finished
-	get_tree().change_scene_to_packed(GAME_SCENE)
+	var game_scene: PackedScene = load("uid://daih55gqatj4l")
+	if beer_can_opening_sound.playing:
+
+		await beer_can_opening_sound.finished
+
+	var error: Error = get_tree().change_scene_to_packed(game_scene)
+	if not error == OK:
+
+		printerr("Failed changing main menu to game scene")
+
+	else:
+
+		print("Main scene instantiated.")
 
 
 func _on_quit_button_pressed() -> void:
