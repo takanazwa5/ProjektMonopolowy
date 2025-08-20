122d6c0846cec464722ca35aebf81f923561f7f7
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-08-20 18:03:00 +0200

    Add target node navigation to NPC script
    Introduces a _target_node variable and a _navigate_to_node function to allow NPCs to navigate to specific Node3D targets. Updates navigation completion logic to handle and reset the target node, with debug print statements for tracking target changes and navigation completion.

--- features/npc/npc.gd
+++ features/npc/npc.gd
@@ -5,6 +5,9 @@ const ROTATION_SPEED: float = 10.0
 const WALKING_SPEED: float = 1.5
 
 
+var _target_node: Node3D
+
+
 @onready var navigation_agent_3d: NavigationAgent3D = %NavigationAgent3D
 @onready var animation_tree: AnimationTree = $AnimationTree
 
@@ -42,6 +45,14 @@ func _process(_delta: float) -> void:
 	animation_tree.set(&"parameters/blend_position", blend_pos)
 
 
+func _navigate_to_node(node: Node3D) -> void:
+
+	if _target_node == node: return
+	_target_node = node
+	print("target changed to: %s" % node)
+	navigation_agent_3d.target_position = node.global_position
+
+
 func _on_target_reached() -> void:
 
 	pass
@@ -55,5 +66,10 @@ func _on_navigation_finished() -> void:
 	var target_transform: Transform3D = transform.looking_at(target_pos, Vector3.UP, true)
 	var tween: Tween = create_tween().set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, ^"transform", target_transform, 0.25)
+	if _target_node is Node3D:
+
+		print("navigation finished - target: %s" % _target_node)
+		_target_node = null
+
 	#print("navigation finished")
 	#print("distance to target: %s" % navigation_agent_3d.distance_to_target())
