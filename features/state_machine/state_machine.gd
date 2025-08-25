e55abb9cdb0d9128c13079d30aacf591b4b4fc20
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-08-25 18:45:29 +0200

    Add editor support and validation to StateMachine
    Added @tool annotation for editor usage, configuration warnings for missing initial state, and editor hint checks to prevent runtime logic from executing in the editor. This improves usability and error feedback when configuring the StateMachine in the Godot editor.

--- features/state_machine/state_machine.gd
+++ features/state_machine/state_machine.gd
@@ -1,7 +1,13 @@
+@tool
 class_name StateMachine extends Node
 
 
-@export var initial_state : State
+@export var initial_state : State:
+
+	set(value):
+
+		initial_state = value
+		update_configuration_warnings()
 
 
 @onready var state : State = initial_state
@@ -9,6 +15,7 @@ class_name StateMachine extends Node
 
 func _ready() -> void:
 
+	if Engine.is_editor_hint(): return
 	assert(initial_state is State, "(%s) initial_state property not set." % get_path())
 
 	for child : Node in get_children():
@@ -22,22 +29,37 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 
+	if Engine.is_editor_hint(): return
 	state.input_event(event)
 
 
 func _process(delta: float) -> void:
 
+	if Engine.is_editor_hint(): return
 	state.update(delta)
 	DebugPanel.add_property(state.name, "State", 100)
 
 
 func _physics_process(delta: float) -> void:
 
+	if Engine.is_editor_hint(): return
 	state.physics_update(delta)
 
 
 func _on_state_transition(new_state: State) -> void:
 
+	if Engine.is_editor_hint(): return
 	state.exit()
 	state = new_state
 	new_state.enter()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+
+	if not initial_state is State:
+
+		return ["Initial State must be set."]
+
+	else:
+
+		return []
