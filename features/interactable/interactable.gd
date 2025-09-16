a2ae9b8f11946eaf74b871ffbe318bda635357a0
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-09-16 21:22:40 +0200

    Make Interactable class abstract and update interact method
    Marked the Interactable class and its interact method as abstract to enforce implementation in subclasses. Simplified the interact method signature by removing unused parameters.

--- features/interactable/interactable.gd
+++ features/interactable/interactable.gd
@@ -1,10 +1,8 @@
-class_name Interactable extends CollisionObject3D
+@abstract class_name Interactable extends CollisionObject3D
 
 
 @export var can_interact: bool = true
 @export var reticle_tooltip_text: String = ""
 
 
-func interact(_event: InputEvent, _item_rig_item: Item) -> void:
-
-	pass
+@abstract func interact(_event: InputEvent, _item_rig_item: Item) -> void
