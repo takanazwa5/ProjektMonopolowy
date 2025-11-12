c686856ac771d5bdbe6c079bccf1b6505173ec98
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-11-12 15:34:42 +0100

    Add interaction metadata to cigarettes and wine items
    Introduces 'can_interact' and 'reticle_tooltip' metadata to all cigarette and wine item scenes, replacing previous tooltip fields. Updates the editor script to support setting interaction metadata, including a dialog for single node selection.

--- editor_scripts/add_interaction.gd
+++ editor_scripts/add_interaction.gd
@@ -9,11 +9,34 @@ func _run() -> void:
 
 	if selected_nodes.is_empty():
 
-		print("No nodes selected.")
-		return
+		selected_nodes.append(EditorInterface.get_edited_scene_root())
 
-	for node: Node in selected_nodes:
+	if selected_nodes.size() == 1:
 
-		node.set_meta(&"can_interact", true)
-		node.set_meta(&"reticle_tooltip", "")
-		print("Added interaction meta to %s" % node.name)
+		var window: Window = Window.new()
+		window.size = Vector2i(500, 30)
+		window.unresizable = true
+		var line_edit: LineEdit = LineEdit.new()
+		line_edit.size = Vector2i(500, 20)
+		line_edit.text_submitted.connect(func(_text: String) -> void:
+			_add_interaction(selected_nodes[0], line_edit.text)
+			window.queue_free()
+		)
+		window.add_child(line_edit)
+
+		window.close_requested.connect(window.queue_free)
+		EditorInterface.popup_dialog_centered(window)
+		line_edit.grab_focus()
+
+	else:
+
+		for node: Node in selected_nodes:
+
+			_add_interaction(node)
+
+
+func _add_interaction(node: Node, reticle_tooltip: String = "") -> void:
+
+	node.set_meta(&"can_interact", true)
+	node.set_meta(&"reticle_tooltip", reticle_tooltip)
+	print("Added interaction meta to %s" % node.name)
