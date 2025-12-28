83935c595518e3da73aa9672492b2066747b0163
R-Miedzinski <radek.miedzinski@gmail.com>
2025-12-28 15:55:05 +0100

    trash spawning location fix

--- features/trash/trash.gd
+++ features/trash/trash.gd
@@ -12,13 +12,12 @@ enum TrashType {
 func interact(event: InputEvent, _item_in_hand: Item) -> void:
   if not event.is_action_pressed(&"interact"): return
 
-  if _item_in_hand != null:
-    if _item_in_hand.get_node("AnimationPlayer") != null:
-      var anim_player: AnimationPlayer = _item_in_hand.get_node("AnimationPlayer")
-      anim_player.play("USE")
-
   if trash_type == TrashType.SOLID:
     emit_signal("disposed", self)
   elif trash_type == TrashType.LIQUID:
     if _item_in_hand != null and _item_in_hand.data.name == "Mop":
       emit_signal("disposed", self)
+      
+      if _item_in_hand.get_node("AnimationPlayer") != null:
+        var anim_player: AnimationPlayer = _item_in_hand.get_node("AnimationPlayer")
+        anim_player.play("USE")
--- features/trash/trash_container.gd
+++ features/trash/trash_container.gd
@@ -4,13 +4,14 @@ signal trash_spawned(trash: Trash)
 
 @export var spawn_height_offset: float = 0.1
 
-var MAX_TRASH_COUNT: int = 10
+@export var MAX_TRASH_COUNT: int = 10
 var current_trash_count: int = 0
 
-var MAX_HELD_TRASH: int = 2
+@export var MAX_HELD_TRASH: int = 2
 var held_trash_count: int = 0
 
 var spawn_points: PackedVector3Array = []
+var trash_registry: Dictionary[Vector2, bool] = {}
 
 var trash_scenes: Array[PackedScene] = [
   preload("res://features/trash/can_rusted/can_rusted.tscn"),
@@ -24,6 +25,9 @@ func _ready() -> void:
   var navigation_region: NavigationRegion3D = self.get_node("../NavigationRegion3D")
   var navigation_mesh_instance: NavigationMesh = navigation_region.navigation_mesh
   self.spawn_points = navigation_mesh_instance.get_vertices()
+  for point in self.spawn_points:
+    var key: Vector2 = Vector2(point.x, point.z)
+    trash_registry[key] = false
 
   self.child_entered_tree.connect(_on_child_entered_tree)
   self.child_exiting_tree.connect(_on_child_exited_tree)
@@ -42,10 +46,21 @@ func spawn_trash(count: int) -> void:
     var trash_scene: PackedScene = self.trash_scenes[randi() % self.trash_scenes.size()]
     var trash_instance: Trash = trash_scene.instantiate() as Trash
 
-    var random_point: Vector3 = self.spawn_points[randi() % self.spawn_points.size()]
+    var available_points: Array[Vector3] = []
+    for point in self.spawn_points:
+      if not trash_registry[Vector2(point.x, point.z)]:
+        available_points.append(point)
+
+    if available_points.size() == 0:
+      push_warning("No available spawn points for trash!")
+      return
+
+    var random_point: Vector3 = available_points[randi() % available_points.size()]
 
     add_child(trash_instance)
     trash_instance.global_transform.origin = random_point + Vector3.DOWN * self.spawn_height_offset
+    var key: Vector2 = Vector2(random_point.x, random_point.z)
+    trash_registry[key] = true
 
     trash_instance.disposed.connect(_on_dispose_trash)
     emit_signal("trash_spawned", trash_instance)
@@ -54,7 +69,10 @@ func _on_child_entered_tree(node: Node) -> void:
   if node is Trash: self.current_trash_count += 1
   
 func _on_child_exited_tree(node: Node) -> void:
-  if node is Trash: self.current_trash_count -= 1
+  if node is Trash:
+    self.current_trash_count -= 1
+    var key: Vector2 = Vector2(node.global_transform.origin.x, node.global_transform.origin.z)
+    trash_registry[key] = false
 
 func _on_dispose_trash(trash: Trash) -> void:
   if trash.trash_type == Trash.TrashType.SOLID:
