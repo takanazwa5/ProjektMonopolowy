58f46e01244f12626f0151d4e6df6cbe0af37c49
R-Miedzinski <radek.miedzinski@gmail.com>
2026-01-06 21:14:07 +0100

    variable name and types fixes

--- features/trash/trash.gd
+++ features/trash/trash.gd
@@ -9,15 +9,16 @@ enum TrashType {
 
 @export var trash_type: TrashType = TrashType.SOLID
   
-func interact(event: InputEvent, _item_in_hand: Item) -> void:
+func interact(event: InputEvent, item_in_hand: Item) -> void:
 	if not event.is_action_pressed(&"interact"): return
 
 	if trash_type == TrashType.SOLID:
 		disposed.emit(self)
+
 	elif trash_type == TrashType.LIQUID:
-		if _item_in_hand != null and _item_in_hand.data.type == ItemData.Type.APPLIANCE and _item_in_hand.data.name == "Mop":
+		if is_instance_valid(item_in_hand) and item_in_hand.data.type == ItemData.Type.APPLIANCE and item_in_hand.data.name == "Mop":
 			disposed.emit(self)
 
-			var anim_player: AnimationPlayer = _item_in_hand.get_node_or_null("AnimationPlayer")
-			if anim_player != null:
+			var anim_player: AnimationPlayer = item_in_hand.get_node_or_null("AnimationPlayer")
+			if is_instance_valid(anim_player):
 				anim_player.play("USE")
--- features/trash/trash_container.gd
+++ features/trash/trash_container.gd
@@ -2,13 +2,19 @@ extends Node3D
 
 signal trash_spawned(trash: Trash)
 
+const TRASH_SCENES: Array[PackedScene] = [
+	preload("res://features/trash/can_rusted/can_rusted.tscn"),
+	preload("res://features/trash/rubber_duck/rubber_duck.tscn"),
+	preload("res://features/trash/dark_stain/dark_stain.tscn"),
+]
+
 @export var spawn_height_offset: float = 0.1
-@export var MAX_TRASH_COUNT: int = 10
-@export var MAX_HELD_TRASH: int = 2
+@export var max_trash_count: int = 10
+@export var max_held_trash: int = 2
 @export_group("Floor Thresholds")
-@export var ROOM_X_THRESHOLDS: Vector2 = Vector2(-1.0, 1.0)
-@export var ROOM_Y_THRESHOLDS: Vector2 = Vector2(0.1, 1.0)
-@export var ROOM_Z_THRESHOLDS: Vector2 = Vector2(-1.0, 1.0)
+@export var room_x_thresholds: Vector2 = Vector2(-1.0, 1.0)
+@export var room_y_thresholds: Vector2 = Vector2(0.1, 1.0)
+@export var room_z_thresholds: Vector2 = Vector2(-1.0, 1.0)
 
 var current_trash_count: int = 0
 var held_trash_count: int = 0
@@ -16,85 +22,79 @@ var held_trash_count: int = 0
 var spawn_points: PackedVector3Array = []
 var trash_registry: Dictionary[Vector2, bool] = {}
 
-var trash_scenes: Array[PackedScene] = [
-  preload("res://features/trash/can_rusted/can_rusted.tscn"),
-  preload("res://features/trash/rubber_duck/rubber_duck.tscn"),
-  preload("res://features/trash/dark_stain/dark_stain.tscn"),
-]
-
+@onready var navigation_region: NavigationRegion3D = %NavigationRegion3D
 @onready var trash_can: TrashCan = %TrashCan
 
 func _ready() -> void:
-  var navigation_region: NavigationRegion3D = self.get_node("../NavigationRegion3D")
-  var navigation_mesh_instance: NavigationMesh = navigation_region.navigation_mesh
-  for point in navigation_mesh_instance.get_vertices():
-    if not _is_point_in_bounds(point): continue # Ignore out of bounds spawn points
-  
-    self.spawn_points.append(point)
-    var key: Vector2 = Vector2(point.x, point.z)
-    self.trash_registry[key] = false
+	var navigation_mesh_instance: NavigationMesh = self.navigation_region.navigation_mesh
+	for point in navigation_mesh_instance.get_vertices():
+		if not _is_point_in_bounds(point): continue # Ignore out of bounds spawn points
+	
+		self.spawn_points.append(point)
+		var key: Vector2 = Vector2(point.x, point.z)
+		self.trash_registry[key] = false
 
-  self.child_entered_tree.connect(_on_child_entered_tree)
-  self.child_exiting_tree.connect(_on_child_exited_tree)
+	self.child_entered_tree.connect(_on_child_entered_tree)
+	self.child_exiting_tree.connect(_on_child_exited_tree)
 
-  self.trash_can.interacted.connect(_empty_held_trash)
+	self.trash_can.interacted.connect(_empty_held_trash)
 
-func _process(delta: float) -> void:
-  DebugPanel.add_property(self.current_trash_count, "Current Trash Count", 100)
-  DebugPanel.add_property("%d/%d" % [self.held_trash_count, self.MAX_HELD_TRASH], "Held Trash", 100)
+func _process(_delta: float) -> void:
+	DebugPanel.add_property(self.current_trash_count, "Current Trash Count", 100)
+	DebugPanel.add_property("%d/%d" % [self.held_trash_count, self.max_held_trash], "Held Trash", 100)
 
 func spawn_trash(count: int) -> void:
-  for i in count:
-    if self.current_trash_count >= self.MAX_TRASH_COUNT:
-      return
+	for i in count:
+		if self.current_trash_count >= self.max_trash_count:
+			return
 
-    var trash_scene: PackedScene = self.trash_scenes.pick_random()
-    var trash_instance: Trash = trash_scene.instantiate() as Trash
+		var trash_scene: PackedScene = self.TRASH_SCENES.pick_random()
+		var trash_instance: Trash = trash_scene.instantiate() as Trash
 
-    var available_points: Array[Vector3] = []
-    for point in self.spawn_points:
-      if not trash_registry[Vector2(point.x, point.z)]:
-        available_points.append(point)
+		var available_points: Array[Vector3] = []
+		for point in self.spawn_points:
+			if not trash_registry[Vector2(point.x, point.z)]:
+				available_points.append(point)
 
-    if available_points.size() == 0:
-      push_warning("No available spawn points for trash!")
-      return
+		if available_points.size() == 0:
+			push_warning("No available spawn points for trash!")
+			return
 
-    var random_point: Vector3 = available_points.pick_random()
+		var random_point: Vector3 = available_points.pick_random()
 
-    add_child(trash_instance)
-    trash_instance.global_position = random_point + Vector3.DOWN * self.spawn_height_offset
-    var key: Vector2 = Vector2(random_point.x, random_point.z)
-    trash_registry[key] = true
+		add_child(trash_instance)
+		trash_instance.global_position = random_point + Vector3.DOWN * self.spawn_height_offset
+		var key: Vector2 = Vector2(random_point.x, random_point.z)
+		trash_registry[key] = true
 
-    trash_instance.disposed.connect(_on_dispose_trash)
-    trash_spawned.emit(trash_instance)
+		trash_instance.disposed.connect(_on_dispose_trash)
+		trash_spawned.emit(trash_instance)
 
 func _on_child_entered_tree(node: Node) -> void:
-  if node is Trash: self.current_trash_count += 1
-  
+	if node is Trash: self.current_trash_count += 1
+	
 func _on_child_exited_tree(node: Node) -> void:
-  if node is Trash:
-    self.current_trash_count -= 1
-    var key: Vector2 = Vector2(node.global_position.x, node.global_position.z)
-    trash_registry[key] = false
+	if node is Trash:
+		self.current_trash_count -= 1
+		var key: Vector2 = Vector2(node.global_position.x, node.global_position.z)
+		trash_registry[key] = false
 
 func _on_dispose_trash(trash: Trash) -> void:
-  if trash.trash_type == Trash.TrashType.SOLID:
-    if self.held_trash_count < self.MAX_HELD_TRASH:
-      self.held_trash_count += 1
-      trash.queue_free()
-  elif trash.trash_type == Trash.TrashType.LIQUID:
-    trash.queue_free()
+	if trash.trash_type == Trash.TrashType.SOLID:
+		if self.held_trash_count < self.max_held_trash:
+			self.held_trash_count += 1
+			trash.queue_free()
+	elif trash.trash_type == Trash.TrashType.LIQUID:
+		trash.queue_free()
 
 func _empty_held_trash() -> void:
-  self.held_trash_count = 0
+	self.held_trash_count = 0
 
 func _is_point_in_bounds(point: Vector3) -> bool:
-  if point.x < ROOM_X_THRESHOLDS.x or point.x > ROOM_X_THRESHOLDS.y:
-    return false
-  if point.y < ROOM_Y_THRESHOLDS.x or point.y > ROOM_Y_THRESHOLDS.y:
-    return false
-  if point.z < ROOM_Z_THRESHOLDS.x or point.z > ROOM_Z_THRESHOLDS.y:
-    return false
-  return true
+	if point.x < room_x_thresholds.x or point.x > room_x_thresholds.y:
+		return false
+	if point.y < room_y_thresholds.x or point.y > room_y_thresholds.y:
+		return false
+	if point.z < room_z_thresholds.x or point.z > room_z_thresholds.y:
+		return false
+	return true
